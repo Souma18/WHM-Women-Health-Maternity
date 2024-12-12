e318790863873371b4f099df5998dbf8ada78c0f
Ngoc Thien aka Subeo <22130262@st.hcmuaf.edu.vn>
2024-12-12 22:17:02 +0700

    upload Medical Profile

--- Document/Font_end/viewShare/addHeaderFooter.js
+++ Document/Font_end/viewShare/addHeaderFooter.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   //tải phần header
-  fetch("/viewShare/header.html")
+  fetch("../viewShare/header.html")
     .then((respone) => respone.text())
     .then((data) => {
       document.getElementById("header").innerHTML = data;
     });
   //tải phần footer
-  fetch("/viewShare/footer.html")
+  fetch("../viewShare/footer.html")
     .then((respone) => respone.text())
     .then((data) => {
       document.getElementById("footer").innerHTML = data;
